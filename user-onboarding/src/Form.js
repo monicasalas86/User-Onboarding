a85ffa37d21f28475ed3ef7327ab4da576d44b44
monicasalas86 <monicsalas167@gmail.com>
2021-09-15 18:22:45 -0500

    styled form

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import StyledForm from './StyledForm'
 
 // form needs name, email, password, terms of service checked, and submit button
 
@@ -17,58 +18,60 @@ export default function Form(props) {
     }
 
     return (
-        <form className='form-container' onSubmit={onSubmit}>
-            <h2>New User Sign Up</h2>
-            
-            {/* errors */}
-            <div className='errors'>
-                <div>{errors.name}</div>
-                <div>{errors.email}</div>
-                <div>{errors.password}</div>
-                <div>{errors.terms}</div>
-            </div>
+        <StyledForm>
+            <form className='form-container' onSubmit={onSubmit}>
+                <h2>New User Sign Up</h2>
+                
+                {/* errors */}
+                <div className='errors'>
+                    <div>{errors.name}</div>
+                    <div>{errors.email}</div>
+                    <div>{errors.password}</div>
+                    <div>{errors.terms}</div>
+                </div>
 
-            <div className='form-inputs'>
-                <label>Name: 
-                    <input
-                        value={values.name}
-                        onChange={onChange}
-                        name='name'
-                        type='text'
-                        placeholder='name'
-                    />
-                </label>
+                <div className='form-inputs'>
+                    <label>Name: 
+                        <input
+                            value={values.name}
+                            onChange={onChange}
+                            name='name'
+                            type='text'
+                            placeholder='name'
+                        />
+                    </label>
 
-                <label>Email: 
-                    <input
-                        value={values.email}
-                        onChange={onChange}
-                        name='email'
-                        type='text'
-                        placeholder='email'
-                    />
-                </label>
+                    <label>Email: 
+                        <input
+                            value={values.email}
+                            onChange={onChange}
+                            name='email'
+                            type='text'
+                            placeholder='email'
+                        />
+                    </label>
 
-                <label>Password: 
-                    <input
-                        value={values.password}
-                        onChange={onChange}
-                        name='password'
-                        type='password'
-                        placeholder='password'
-                    />
-                </label>
+                    <label>Password: 
+                        <input
+                            value={values.password}
+                            onChange={onChange}
+                            name='password'
+                            type='password'
+                            placeholder='password'
+                        />
+                    </label>
 
-                <label>Terms of Service: 
-                    <input
-                        type='checkbox'
-                        name='terms'
-                        checked={values.terms}
-                        onChange={onChange}
-                    />
-                </label>
-                <button disabled={disabled}>Sumbit</button>
-            </div>
-        </form>
+                    <label>Terms of Service: 
+                        <input
+                            type='checkbox'
+                            name='terms'
+                            checked={values.terms}
+                            onChange={onChange}
+                        />
+                    </label>
+                    <button disabled={disabled}>Sumbit</button>
+                </div>
+            </form>
+        </StyledForm>
     )
 }
